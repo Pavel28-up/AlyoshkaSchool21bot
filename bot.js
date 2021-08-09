3125c590347999e29a5e407b01c3057f31a4d80d
Aleksey Skhomenko <a@skhom.ru>
2021-08-09 20:07:49 +0700

    new patch 09.08.2021

--- bot.js
+++ bot.js
@@ -1,15 +1,66 @@
-require('dotenv').config()
-const {Telegraf} = require('telegraf')
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const TIMEOUT = 300000;
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}! Помощь /help`))
-bot.help((ctx) => ctx.reply('Чем могу помочь?\n флудилка - /chat\n Информация о ПЦР, прививках, методы тестирования - /synopsis\n вопросы и хештеги - /hashtag\n Хочешь стикеры? Приведи друга! Реферальная программа - /sticker\n Бонусы для студентов, плюшки, хостелы - /bonus\n Флаеры на скидку 20% в Инвитро - /flyer\n Даты бассейнов, адрес доставки документов, время работы - /docs'))
-bot.hears('Привет', (ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}! Помощь /help`))
-bot.command('chat', (ctx) => ctx.reply('Общая флудилка https://t.me/c/1246298368/631 \n Август https://t.me/c/1246298368/4802 \n Сентябрь:https://t.me/c/1246298368/4808 '))
-bot.command('synopsis', (ctx) => ctx.reply('https://t.me/c/1246298368/2496'))
-bot.command('hashtag', (ctx) => ctx.reply('https://t.me/c/1246298368/1904'))
-bot.command('sticker', (ctx) => ctx.reply('https://t.me/c/1246298368/3906 \n https://t.me/c/1246298368/3907'))
-bot.command('bonus', (ctx) => ctx.reply(' https://t.me/c/1246298368/3294'))
-bot.command('flyer', (ctx) => ctx.reply(' https://t.me/c/1246298368/3489'))
-bot.command('docs', (ctx) => ctx.reply(' https://t.me/c/1246298368/102 \n https://t.me/c/1246298368/5218 (документы несем сюда!)'))
-bot.launch()
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// триггеры:
+bot.hears('Привет', (ctx) => {
+    ctx.reply(`Привет ${ctx.message.from.first_name}! Помощь /help`).then(msg => {
+        setTimeout(() => { bot.telegram.deleteMessage(msg.chat.id, msg.message_id) }, TIMEOUT);
+    });
+});
+
+// команды бота:
+bot.help((ctx) => {
+    if (ctx.chat.type === 'private') {
+        ctx.reply('Чем могу помочь?\n флудилка - /chat\n Информация о ПЦР, прививках, методы тестирования - /synopsis\n вопросы и хештеги - /hashtag\n Хочешь стикеры? Приведи друга! Реферальная программа - /sticker\n Бонусы для студентов, плюшки, хостелы - /bonus\n Флаеры на скидку 20% в Инвитро - /flyer\n Даты бассейнов, адрес доставки документов, время работы - /docs');
+    }
+});
+bot.start((ctx) => {
+    if (ctx.chat.type === 'private') {
+        ctx.reply(`Привет ${ctx.message.from.first_name}! Помощь /help`);
+    }
+});
+bot.command('chat', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        ctx.reply('Общая флудилка https://t.me/c/1246298368/631 \n Август https://t.me/c/1246298368/4802 \n Сентябрь:https://t.me/c/1246298368/4808 ');
+    }
+});
+bot.command('synopsis', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        // ctx.reply('https://t.me/c/1246298368/2496');
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 2496, {disable_notification: true});
+    }
+});
+bot.command('hashtag', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        // ctx.reply('https://t.me/c/1246298368/1904');
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 1904, {disable_notification: true});
+    }
+});
+bot.command('sticker', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        ctx.reply('https://t.me/c/1246298368/3906 \n https://t.me/c/1246298368/3907');
+    }
+});
+bot.command('bonus', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        // ctx.reply('https://t.me/c/1246298368/3294');
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 3294, {disable_notification: true});
+    }
+});
+bot.command('flyer', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        // ctx.reply('https://t.me/c/1246298368/3489');
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 3489, {disable_notification: true});
+    }
+});
+bot.command('docs', (ctx) => {
+    if (ctx.chat.type === 'private') {
+        // ctx.reply('https://t.me/c/1246298368/102 \n https://t.me/c/1246298368/5218 (документы несем сюда!)');
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 102, {disable_notification: true});
+        bot.telegram.forwardMessage(ctx.update.message.chat.id, -1001246298368, 5218, {disable_notification: true});
+    }
+});
+bot.launch();
